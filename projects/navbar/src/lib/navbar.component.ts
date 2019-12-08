e7ad69d9dbdcad7f22a517a6c1db4d1e5f7407b3
michael-spengler <>
2019-12-08 14:24:20 +0100

    navbar

--- projects/navbar/src/lib/navbar.component.ts
+++ projects/navbar/src/lib/navbar.component.ts
@@ -1,6 +1,12 @@
 import { Component, Input, Output, EventEmitter, OnInit, NgZone, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
 
+export interface INavbarData {
+  cssClass: string;
+  text: string;
+  href: string;
+}
+
 @Component({
   selector: 'lib-navbar',
   templateUrl: './navbar.component.html',
@@ -10,38 +16,16 @@ export class NavbarComponent implements OnInit {
   public static firstFancyCity = '';
   public static forFreePartners = [];
 
-  constructor() {
-    this.titleLong = 'Dance Planner';
-
-  }
+  constructor() {}
 
 
-  @Input() public titleLong = 'Test';
-  @Input() texts: string[] = [];
+  @Input() public titleLong = 'Your Title';
+  @Input() navBarData: INavbarData[] = [];
   @Output() showUseAsAppDetail = new EventEmitter<void>();
   @Output() findDanceSchool = new EventEmitter<void>();
-  // public titleShort;
-  // public famousCounter = 0;
-  // public displayInput = false;
-  // public adminkey = '';
   public readyForPrompt: boolean;
 
-
-
-  public ngOnInit() {
-
-
-    setTimeout(() => {
-        this.titleLong = `Dance Planner ${NavbarComponent.firstFancyCity}`;
-      }, 7000);
-    // setTimeout(() => {
-    //   this.backendService.getForFreePartners()
-    //     .subscribe((result) => {
-    //       NavbarComponent.forFreePartners =  result;
-    //     });
-    //   }, 60000);
-
-  }
+  public ngOnInit() {}
 
   public myFunction() {
     const x = document.getElementById('myTopnav');
@@ -52,181 +36,7 @@ export class NavbarComponent implements OnInit {
     }
   }
 
-  public showErgebnisseFiltern() {
-    return document.documentElement.scrollTop === 0 ? false : true;
-  }
-
-  public clickFind() {
-    // if (this.router.url === '/') {
-    //   this.router.navigateByUrl('/tips-and-tricks');
-    //   setTimeout(() => {
-    //     this.router.navigateByUrl('/');
-    //   }, 100);
-    // } else {
-    //   this.router.navigateByUrl('/');
-    // }
-  }
-
-  public clickPlaylistsAndTutorials() {
-    // if (this.router.url === '/playlistsAndTutorials') {
-    //   this.router.navigateByUrl('/tips-and-tricks');
-    //   setTimeout(() => {
-    //     this.router.navigateByUrl('/playlistsAndTutorials');
-    //   }, 100);
-    // } else {
-    //   this.router.navigateByUrl('/playlistsAndTutorials');
-    // }
-  }
-
-  public clickPlaylists() {
-    // if (this.router.url === '/playlists') {
-    //   this.router.navigateByUrl('/tips-and-tricks');
-    //   setTimeout(() => {
-    //     this.router.navigateByUrl('/playlists');
-    //   }, 100);
-    // } else {
-    //   this.router.navigateByUrl('/playlists');
-    // }
-  }
-
-  public clickFurtherFunctions() {
-    // if (this.router.url === '/furtherFunctions') {
-    //   this.router.navigateByUrl('/tips-and-tricks');
-    //   setTimeout(() => {
-    //     this.router.navigateByUrl('/furtherFunctions');
-    //   }, 100);
-    // } else {
-    //   this.router.navigateByUrl('/furtherFunctions');
-    // }
-  }
-
-  public clickFindDanceSchool() {
-    // if (this.router.url === '/danceSchools') {
-    //   this.router.navigateByUrl('/tips-and-tricks');
-    //   setTimeout(() => {
-    //     this.router.navigateByUrl('/danceSchools');
-    //   }, 100);
-    // } else {
-    //   this.router.navigateByUrl('/danceSchools');
-    // }
-  }
-
-  public clickUseAsApp() {
-    // if (NavbarComponent.useAsApp()) {
-    //   // relax
-    // } else {
-    //   if (this.router.url !== '/') {
-    //     this.router.navigateByUrl('/?useAsApp=true');
-    //   } else {
-    //     this.myFunction();
-    //     this.showUseAsAppDetail.emit();
-    //   }
-    // }
-  }
-
-
-  public clickTutorials() {
-    // if (this.router.url === '/tutorials') {
-    //   this.router.navigateByUrl('/tips-and-tricks');
-    //   setTimeout(() => {
-    //     this.router.navigateByUrl('/tutorials');
-    //   }, 100);
-    // } else {
-    //   this.router.navigateByUrl('/tutorials');
-    // }
-  }
-
-  public clickNewEvent() {
-    // if (this.router.url === '/new-event') {
-    //   this.router.navigateByUrl('/tips-and-tricks');
-    //   setTimeout(() => {
-    //     this.router.navigateByUrl('/new-event');
-    //   }, 100);
-    // } else {
-    //   this.router.navigateByUrl('/new-event');
-    // }
-  }
-
-  public clickForFree() {
-    // if (this.router.url === '/for-free') {
-    //   this.router.navigateByUrl('/tips-and-tricks');
-    //   setTimeout(() => {
-    //     this.router.navigateByUrl('/for-free');
-    //   }, 11);
-    // } else {
-    //   this.router.navigateByUrl('/for-free');
-    // }
-  }
-
-  public clickFindPAndC() {
-    // this.router.navigateByUrl('/partnerAndCommunity');
-  }
-
-  public clickCommunity() {
-    // if (this.router.url === '/community') {
-    //   this.router.navigateByUrl('/tips-and-tricks');
-    //   setTimeout(() => {
-    //     this.router.navigateByUrl('/community');
-    //   }, 11);
-    // } else {
-    //   this.router.navigateByUrl('/community');
-    // }
-  }
-
-  public clickAroundTheWorld() {
-    // if (this.router.url === '/aroundTheWorld') {
-    //   this.router.navigateByUrl('//tips-and-tricks');
-    //   setTimeout(() => {
-    //     this.router.navigateByUrl('/aroundTheWorld');
-    //   }, 11);
-    // } else {
-    //   this.router.navigateByUrl('/aroundTheWorld');
-    // }
-  }
-
-  public clickTipsAndTricks() {
-    // if (this.router.url === '/tips-and-tricks') {
-    //   this.router.navigateByUrl('/community');
-    //   setTimeout(() => {
-    //     this.router.navigateByUrl('/tips-and-tricks');
-    //   }, 11);
-    // } else {
-    //   this.router.navigateByUrl('/tips-and-tricks');
-    // }
-  }
-
-  public clickContact() {
-    // if (this.router.url === '/contact') {
-    //   this.router.navigateByUrl('/tips-and-tricks');
-    //   setTimeout(() => {
-    //     this.router.navigateByUrl('/contact');
-    //   }, 11);
-    // } else {
-    //   this.router.navigateByUrl('/contact');
-    // }
-  }
 
   public clickTitle() {
-    // document.body.scrollTop = 0;
-    // document.documentElement.scrollTop = 0;
-    // this.famousCounter++;
-    // setTimeout(() => {
-    //   if (this.famousCounter === 1) {
-    //     this.router.navigateByUrl('/tips-and-tricks');
-    //     setTimeout(() => {
-    //       this.router.navigateByUrl('/');
-    //     }, 2);
-    //     this.famousCounter++;
-    //   } else if (this.famousCounter === 5) {
-    //     this.router.navigateByUrl(`/corrections`);
-    //   } else if (this.famousCounter === 4) {
-    //     this.router.navigateByUrl(`/admin`);
-    //   } else if (this.famousCounter === 3) {
-    //     this.router.navigateByUrl(`/supporter`);
-    //   }
-    // }, 1000);
-  }
-  public useYourFavoriteLanguage() {
-    // this.router.navigateByUrl(`/community-translation`);
   }
 }
--- projects/test/src/app/app.component.ts
+++ projects/test/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { IImageLinkWithDetailsLink } from '../../../images-grid/src/lib/images-grid.interfaces';
 import { IContactData } from '../../../contact-options/src/public-api';
+import { IIndividualImpressumData } from '../../../impressum/src/public-api';
+import { IDPSData } from '../../../dps/src/public-api';
+import { INavbarData } from '../../../navbar/src/public-api';
 
 @Component({
   selector: 'app-root',
@@ -8,39 +10,67 @@ import { IContactData } from '../../../contact-options/src/public-api';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'test';
-  imageLinksWithDetailLinks: IImageLinkWithDetailsLink[] = this.getData();
-  public contactOptions: IContactData[] = [];
+  public title = 'Fancy Cars';
+  public navBarData: INavbarData[] = this.getNavBarData();
+  public individualImpressumData: IIndividualImpressumData = this.getImpressumData();
+  public contactOptions: IContactData[] = this.getContactData();
+  public individualDPSData: IDPSData = this.getDPSData();
+
 
   public ngOnInit() {
-    this.prepareContactData();
+
   }
 
-  private prepareContactData() {
-    this.contactOptions.push({
+  private getNavBarData(): any {
+    return [{
+      cssClass: 'active',
+      href: '/',
+      text: 'Startseite'
+    },
+    {
+      cssClass: '',
+      href: '/models',
+      text: 'Modelle'
+    },
+    {
+      cssClass: '',
+      href: '/dashboard',
+      text: 'Dashboard'
+    },
+    {
+      cssClass: 'right',
+      href: '/about',
+      text: 'Über Uns'
+    }];
+  }
+
+  private getContactData(): IContactData[] {
+    const contactData: IContactData[] = [];
+    contactData.push({
       hyperLink: 'https://www.facebook.com/michael.spengler.5686',
       iconClass: 'fa fa-facebook-official fa-3x',
       padding: '0'
     });
 
-    this.contactOptions.push({
+    contactData.push({
       hyperLink: 'https://chat.whatsapp.com/FNVd44Xn6TfGgzTf6PE2S0',
       iconClass: 'fa fa-whatsapp fa-3x',
       padding: '1.7'
     });
 
-    this.contactOptions.push({
+    contactData.push({
       hyperLink: 'https://t.me/joinchat/CocyExVjtoAZARi6HCtmZA',
       iconClass: 'fa fa-telegram fa-3x',
       padding: '1.7'
     });
 
-    this.contactOptions.push({
+    contactData.push({
       hyperLink: 'mailto:info@dance-planner.de',
       iconClass: 'fa fa-envelope-o fa-3x',
       padding: '1.7'
     });
 
+    return contactData;
   }
 
   private getData() {
@@ -319,6 +349,27 @@ export class AppComponent implements OnInit {
       }
 
     ];
+  }
+
+  private getDPSData(): IDPSData {
+    return {
+      textAlign: 'center',
+      bgColor: 'inherit'
+    };
+  }
+
+  private getImpressumData(): IIndividualImpressumData {
+    return {
+      name: 'Michael Spengler',
+      street: 'Zollhofgarten 8',
+      extension: 'Wohnung 401',
+      zipCode: '69115',
+      city: 'Heidelberg',
+      phoneNumber: '0049 67 83 38 69',
+      eMail: 'michael@fancy-cars.org',
+      textAlign: 'center',
+      bgColor: 'inherit',
+    };
   }
 
 }
