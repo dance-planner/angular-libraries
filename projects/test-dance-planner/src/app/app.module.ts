3f99c6cb374850df58b06e3763f9df1609fe8f7a
michael-spengler <>
2019-12-22 00:21:46 +0100

    using the dance-planner-cards library now

--- projects/test-dance-planner/src/app/app.module.ts
+++ projects/test-dance-planner/src/app/app.module.ts
@@ -7,21 +7,19 @@ import { DpsModule } from '../../../dps/src/public-api';
 import { ImpressumModule } from '../../../impressum/src/public-api';
 import { CardsModule } from '../../../cards/src/public-api';
 import { HttpClientModule } from '@angular/common/http';
-import { EventCardsComponent } from './event-cards/event-cards.component';
 import { ScrollingModule } from '@angular/cdk/scrolling';
-import { EventCardComponent } from './event-card/event-card.component';
 import { SelectionTypeaheadModule } from '../../../selection-typeahead/src/public-api';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ContactModule } from 'dance-planner-contact';
+import { DancePlannerCardsModule } from 'dance-planner-cards';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventCardsComponent,
-    EventCardComponent,
   ],
   imports: [
+    DancePlannerCardsModule,
     ContactModule,
     NgbModule,
     FormsModule,
